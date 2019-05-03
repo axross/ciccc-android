47d291ed85086d41865d80872e09fd7a92b95f0e
Kohei Asai <me@axross.io>
2019-05-02 21:26:18 -0700

    Task 2

--- HelloCompat/app/src/main/java/app/axross/ciccc/hellocompat/MainActivity.java
+++ HelloCompat/app/src/main/java/app/axross/ciccc/hellocompat/MainActivity.java
@@ -2,12 +2,34 @@ package app.axross.ciccc.hellocompat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView helloTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        helloTextView = findViewById(R.id.hello_textview);
+
+        helloTextView.setOnClickListener((View view) -> onChangeColorButtonClick(view));
+
+        if (savedInstanceState != null) {
+            helloTextView.setTextColor(savedInstanceState.getInt("color"));
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("color", helloTextView.getCurrentTextColor());
+    }
+
+    void onChangeColorButtonClick(View view) {
+
     }
 }
