7505ff7926c92aa47e3e1f5527ebfadb0e98894f
Kohei Asai <me@axross.io>
2019-05-07 17:11:27 -0700

    Lab 04.2 (#3)
    * Task 1
    
    * Task 2
    
    * Coding challenge
    
    * Task 3
    
    * Coding challenge 2

--- DroidCafe/app/src/main/java/app/axross/ciccc/droidcafe/OrderActivity.java
+++ DroidCafe/app/src/main/java/app/axross/ciccc/droidcafe/OrderActivity.java
@@ -1,11 +1,23 @@
 package app.axross.ciccc.droidcafe;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class OrderActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity implements
+        AdapterView.OnItemSelectedListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +26,103 @@ public class OrderActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String message = "Order: " + intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         ((TextView) findViewById(R.id.order_textview)).setText(message);
+
+        ((RadioButton) findViewById(R.id.sameday)).setOnClickListener((view) -> onRadioButtonClick(view));
+        ((RadioButton) findViewById(R.id.nextday)).setOnClickListener((view) -> onRadioButtonClick(view));
+        ((RadioButton) findViewById(R.id.pickup)).setOnClickListener((view) -> onRadioButtonClick(view));
+
+        ((RadioButton) findViewById(R.id.nextday)).setChecked(true);
+
+        Spinner spinner = findViewById(R.id.label_spinner);
+        if (spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+        }
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.labels_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_dropdown_item);
+
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
+
+        EditText editText = findViewById(R.id.phone_text);
+        if (editText != null) {
+            editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                    boolean handled = false;
+                    if (actionId == EditorInfo.IME_ACTION_SEND) {
+                        dialNumber();
+                        handled = true;
+                    }
+
+                    return handled;
+                }
+
+                private void dialNumber() {
+                    EditText editText = findViewById(R.id.phone_text);
+                    String phoneNum = null;
+                    if (editText != null) phoneNum = "tel:" +
+                            editText.getText().toString();
+                    Log.d("Phone", "dialNumber: " + phoneNum);
+                    Intent intent = new Intent(Intent.ACTION_DIAL);
+                    intent.setData(Uri.parse(phoneNum));
+                    if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    } else {
+                        Log.d("ImplicitIntents", "Can't handle this!");
+                    }
+                }
+            });
+        }
+
+    }
+
+    private void onRadioButtonClick(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()) {
+            case R.id.sameday:
+                if (checked) {
+                    displayToast(getString(R.string.same_day_messenger_service));
+                }
+
+                break;
+            case R.id.nextday:
+                if (checked) {
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                }
+
+                break;
+            case R.id.pickup:
+                if (checked) {
+                    displayToast(getString(R.string.pick_up));
+                }
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String spinnerLabel = adapterView.getItemAtPosition(i).toString();
+        displayToast(spinnerLabel);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(
+            getApplicationContext(),
+            message,
+            Toast.LENGTH_SHORT
+        ).show();
     }
 }
