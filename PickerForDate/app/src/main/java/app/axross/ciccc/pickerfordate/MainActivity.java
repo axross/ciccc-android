ed72960cf4617b1107a293bfaf2211d1509880f2
Kohei Asai <me@axross.io>
2019-05-08 10:30:50 -0700

    Task 5

---
+++ PickerForDate/app/src/main/java/app/axross/ciccc/pickerfordate/MainActivity.java
@@ -0,0 +1,34 @@
+package app.axross.ciccc.pickerfordate;
+
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button dateButton = findViewById(R.id.date_button);
+        dateButton.setOnClickListener((view) -> onDateButtonClick(view));
+    }
+
+    private void onDateButtonClick(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(),"datePicker");
+    }
+
+    public void processDatePickerResult(int year, int month, int day) {
+        String month_string = Integer.toString(month+1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = (month_string + "/" + day_string + "/" + year_string);
+
+        Toast.makeText(this, "Date: " + dateMessage, Toast.LENGTH_SHORT).show();
+    }
+}
